d23a0312b8f7a2270cd071039cb6dbd1ba9f03ab
Alejandro Puente <alejandro@glooko.com>
2016-12-04 13:22:14 -0800

    added temporary home screen

---
+++ CityCycler/CityCycler/MenuViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MenuViewController.swift
+//  CityCycler
+//
+//  Created by Alejandro Puente on 12/4/16.
+//  Copyright © 2016 Alejandro Puente. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    @IBAction func PlayButton(_ sender: AnyObject) {
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
